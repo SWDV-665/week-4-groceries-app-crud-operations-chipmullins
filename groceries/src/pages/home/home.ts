b178c6ffa2633f4117fb0d99b3c43bf0ff75e4e0
Chip Mullins <62738705+chipmullins@users.noreply.github.com>
2022-07-31 20:18:25 -0400

    Week 4

---
+++ groceries/src/pages/home/home.ts
@@ -0,0 +1,59 @@
+import { Component, Input } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery List";
+
+  
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public inputDialogService: InputDialogServiceProvider) {
+
+  }
+
+  loadItems() {
+    return this.dataService.getItems();
+  }
+
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item -- ' + index + "...",
+      duration: 3000
+    });
+    toast.present();
+
+
+    this.dataService.removeItem(index);
+    
+  }
+
+  editItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Edit Item -- ' + index + "...",
+      duration: 3000
+    });
+    toast.present();
+    this.inputDialogService.showPrompt(item, index);
+
+    
+  }
+
+
+  addItem() {
+    console.log("Adding Item");
+    this.inputDialogService.showPrompt();
+  }
+
+  
+
+}
